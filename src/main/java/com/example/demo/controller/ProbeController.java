c239f3e31241ebea44beb1f3a33c5add2baca5d6
dpoljak2 <dominik.poljak@croz.net>
2025-03-16 00:09:18 +0100

    feature_1_probe_rest_api: controller cleanup via mappers * feature: introduced mapstruct for controller cleanup

--- src/main/java/com/example/demo/controller/ProbeController.java
+++ src/main/java/com/example/demo/controller/ProbeController.java
@@ -6,39 +6,36 @@ import com.example.api.model.generated.V1ProbeInitPost201Response;
 import com.example.api.model.generated.V1ProbeInitPostRequest;
 import com.example.api.model.generated.V1ProbeProbeIdAuditGet200ResponseInner;
 import com.example.api.model.generated.V1ProbeProbeIdAutopilotPostRequest;
-import com.example.api.model.generated.V1ProbeProbeIdAutopilotPostRequestDestination;
 import com.example.api.model.generated.V1ProbeProbeIdCommandPostRequest;
-import com.example.demo.db_entity.Grid;
 import com.example.demo.db_entity.ProbeVisitedPosition;
-import com.example.demo.model.Direction;
+import com.example.demo.dto.ProbeData;
+import com.example.demo.mapper.ProbeMapper;
+import com.example.demo.mapper.ProbePositionMapper;
+import com.example.demo.mapper.ProbeVisitedPositionsMapper;
 import com.example.demo.model.Position;
 import com.example.demo.service.ProbeService;
 import com.example.demo.service.SyncService;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.NotImplementedException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReentrantLock;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.NotImplementedException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class ProbeController implements V1Api {
 
   private final ConcurrentHashMap<Integer, ReentrantLock> probeLocks = new ConcurrentHashMap<>();
 
-  @Autowired
-  private SyncService syncService;
-
-  @Autowired private ProbeService probeService;
+  private final SyncService syncService;
+  private final ProbeService probeService;
+  private final ProbeMapper probeMapper;
+  private final ProbeVisitedPositionsMapper probeVisitedPositionsMapper;
+  private final ProbePositionMapper probePositionMapper;
 
   @Override
   public ResponseEntity<Void> v1VerifyGet() {
@@ -54,121 +51,52 @@ public class ProbeController implements V1Api {
   public ResponseEntity<V1ProbeInitPost201Response> v1ProbeInitPost(
       V1ProbeInitPostRequest request) {
     log.info("Entering v1ProbeInitPost with request: {}", request);
-    List<String> obstacles =
-        request.getObstacles().stream()
-            .map(obstacle -> obstacle.getX() + "," + obstacle.getY())
-            .toList();
-
-    Grid grid =
-        Grid.builder()
-            .xSize(request.getGridSize().getWidth())
-            .ySize(request.getGridSize().getHeight())
-            .obstacles(obstacles)
-            .build();
-    Direction direction = Direction.valueOf(request.getInitialPosition().getDirection().name());
-    Position position =
-        new Position(request.getInitialPosition().getX(), request.getInitialPosition().getY());
-    int probeId = probeService.initialize(grid, position, direction);
+    ProbeData probeData = probeMapper.toProbeData(request);
+    int probeId = probeService.initialize(probeData);
     V1ProbeInitPost201Response body = new V1ProbeInitPost201Response();
     body.setProbeId(probeId);
-    ResponseEntity<V1ProbeInitPost201Response> response = ResponseEntity.ok().body(body);
-    log.info(
-        "v1ProbeInitPost successful with response: {}, HTTP status: {}",
-        response.getBody(),
-        response.getStatusCodeValue());
-    return response;
+    log.info("v1ProbeInitPost successful with response OK, probeId={}", probeId);
+    return ResponseEntity.ok(body);
   }
-
 
   @Override
-  public ResponseEntity<Void> v1ProbeProbeIdCommandPost(Integer probeId,
-    V1ProbeProbeIdCommandPostRequest v1ProbeCommandPostRequest) {
+  public ResponseEntity<Void> v1ProbeProbeIdCommandPost(
+      Integer probeId, V1ProbeProbeIdCommandPostRequest v1ProbeCommandPostRequest) {
     log.info("Entering v1ProbeCommandPost with request: {}", v1ProbeCommandPostRequest);
     String command = v1ProbeCommandPostRequest.getCommand();
-
     // Execution is locked per probeId since all commands for same probe can be executed 1 at a time
     syncService.synchronize(probeId, probeService::executeCommand, probeId, command);
-    ResponseEntity<Void> response = ResponseEntity.ok().build();
-    log.info(
-        "v1ProbeCommandPost successful with response: {}, HTTP status: {}",
-        response.getBody(),
-        response.getStatusCodeValue());
-    return response;
+    log.info("v1ProbeCommandPost successful with response OK");
+    return ResponseEntity.ok().build();
   }
-
-
 
   @Override
   public ResponseEntity<ProbeState> v1ProbeProbeIdStatusGet(Integer probeId) {
     log.info("Entering v1ProbeStatusGet");
     ResponseEntity<ProbeState> response = V1Api.super.v1ProbeProbeIdStatusGet(probeId);
-    log.info(
-        "v1ProbeStatusGet successful with response: {}, HTTP status: {}",
-        response.getBody(),
-        response.getStatusCodeValue());
+    log.info("v1ProbeStatusGet successful with response OK");
     return response;
   }
 
-
   @Override
-  public ResponseEntity<List<V1ProbeProbeIdAuditGet200ResponseInner>> v1ProbeProbeIdAuditGet(Integer probeId) {
+  public ResponseEntity<List<V1ProbeProbeIdAuditGet200ResponseInner>> v1ProbeProbeIdAuditGet(
+      Integer probeId) {
     log.info("Entering v1ProbeAuditGet");
     List<ProbeVisitedPosition> visitedPositions = probeService.audit(probeId);
-    List<V1ProbeProbeIdAuditGet200ResponseInner> responseList = mapToResponse(visitedPositions);
-    ResponseEntity<List<V1ProbeProbeIdAuditGet200ResponseInner>> response =
-        ResponseEntity.ok(responseList);
-    log.info(
-        "v1ProbeAuditGet successful with response: {}, HTTP status: {}",
-        response.getBody(),
-        response.getStatusCodeValue());
-    return response;
+    List<V1ProbeProbeIdAuditGet200ResponseInner> responseList =
+        probeVisitedPositionsMapper.mapToResponse(visitedPositions);
+    log.info("v1ProbeAuditGet successful with response OK, visitedPositions={}", visitedPositions);
+    return ResponseEntity.ok(responseList);
   }
-
 
   @Override
-  public ResponseEntity<Void> v1ProbeProbeIdAutopilotPost(Integer probeId,
-      V1ProbeProbeIdAutopilotPostRequest v1ProbeAutopilotPostRequest) {
+  public ResponseEntity<Void> v1ProbeProbeIdAutopilotPost(
+      Integer probeId, V1ProbeProbeIdAutopilotPostRequest v1ProbeAutopilotPostRequest) {
     log.info("Entering v1ProbeAutopilotPost");
-    V1ProbeProbeIdAutopilotPostRequestDestination destination = v1ProbeAutopilotPostRequest.getDestination();
-    Position destinationPosition = new Position(destination.getX(), destination.getY());
+    Position position = probePositionMapper.toPosition(v1ProbeAutopilotPostRequest);
     // Execution is locked per probeId since all commands for same probe can be executed 1 at a time
-    syncService.synchronize(probeId, probeService::executeAutopilot, probeId, destinationPosition);
-    ResponseEntity<Void> response = ResponseEntity.ok().build();
-    log.info(
-        "v1ProbeAutopilotPost successful with response: {}, HTTP status: {}",
-        response.getBody(),
-        response.getStatusCodeValue());
-    return response;
-  }
-
-  private static List<V1ProbeProbeIdAuditGet200ResponseInner> mapToResponse(
-      List<ProbeVisitedPosition> visitedPositions) {
-    List<V1ProbeProbeIdAuditGet200ResponseInner> responseList =
-        visitedPositions.stream()
-            .map(
-                visitedPosition -> {
-                  V1ProbeProbeIdAuditGet200ResponseInner responseInner =
-                      new V1ProbeProbeIdAuditGet200ResponseInner();
-                  responseInner.setUser(visitedPosition.getUsername());
-                  responseInner.setX(visitedPosition.getXCoordinate());
-                  responseInner.setY(visitedPosition.getYCoordinate());
-                  V1ProbeProbeIdAuditGet200ResponseInner.DirectionEnum directionEnum =
-                    V1ProbeProbeIdAuditGet200ResponseInner.DirectionEnum.valueOf(
-                          visitedPosition.getDirection().name());
-                  responseInner.setDirection(directionEnum);
-                  responseInner.setCommand(visitedPosition.getCommandExecuted());
-
-                  OffsetDateTime timestamp = convertTime(visitedPosition.getTimestampVisited());
-                  responseInner.setTimestamp(timestamp);
-                  return responseInner;
-                })
-            .toList();
-    return responseList;
-  }
-
-  private static OffsetDateTime convertTime(LocalDateTime localDateTime) {
-    ZoneId zoneId = ZoneId.systemDefault();
-    ZoneOffset zoneOffset = zoneId.getRules().getOffset(Instant.now());
-    return localDateTime.atOffset(zoneOffset);
+    syncService.synchronize(probeId, probeService::executeAutopilot, probeId, position);
+    log.info("v1ProbeAutopilotPost successful with response OK");
+    return ResponseEntity.ok().build();
   }
 }
---
+++ src/main/java/com/example/demo/dto/ProbeData.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import com.example.demo.db_entity.Grid;
+import com.example.demo.model.Direction;
+import com.example.demo.model.Position;
+import lombok.Data;
+
+@Data
+public class ProbeData {
+
+  private Grid grid;
+  private Position position;
+  private Direction direction;
+}
